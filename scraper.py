7977634154892ac78dca1276742d15c388639f69
稲葉遼一 <ryoichiinaba@Erio.local>
2018-05-05 16:43:19 +0900

    THRTV

--- scraper.py
+++ scraper.py
@@ -7,16 +7,20 @@ class Scraper:
         self.site = site
 
     def scrape(self):
-        r = urllib.request.urlopen(self.site)
-        html = r.read()
+        response = urllib.request.urlopen(self.site)
+        html = response.read()
         parser = "html.parser"
         sp = BeautifulSoup(html, parser)
-        for tag in sp.find_all("a"):
-            url = tag.get("href")
-            if url is None:
-                continue
-            if "html" in url:
-                print("\n" + url)
+        tables = sp.find_all("table")[3]
+        rows = tables.find_all("tr")
+        with open("output.txt", "w") as f:
+            for row in rows:
+                for name in row.find_all("td", attrs={"class":"s14_24"}):
+                    if name.get_text() is None:
+                        continue
+                    else:
+                        print("\n" + name.get_text())
+                        f.write(name.get_text() + "\n")
 
-news = "https://news.google.com/"
+news = "http://www.shugiintv.go.jp/jp/index.php?ex=VL&u_day=20180502"
 Scraper(news).scrape()
